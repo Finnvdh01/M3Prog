aba46b4f32b8b6ef63c2b9a003ae642d6f391c1b
Finnvdh01 <39246@ma-web.nl>
2025-02-13 12:55:30 +0100

    commit 11

---
+++ m3prog_project/public/01/datatypes.php
@@ -0,0 +1,12 @@
+<?php
+$huisnummer = 123;
+$uitslag = "je bent geslaagd";
+$prijs = 20.50;
+
+echo $huisnummer;
+echo "<br>";
+echo $uitslag;
+echo "<br>";
+echo $prijs;
+echo "<br>";
+?>
---
+++ m3prog_project/public/01/showtypes.php
@@ -0,0 +1,18 @@
+<?php
+
+$nummer = 89;
+$salaris = 0.5;
+$zin = "hello zin";
+$waarofNiet=false;
+$rijtje = [1,2,3,4,5];
+
+echo gettype($nummer);
+echo "<br>";
+echo gettype($salaris);
+echo "<br>";
+echo gettype($zin);
+echo "<br>";
+echo gettype($waarofNiet);
+echo "<br>";
+echo gettype($rijtje);
+?>
