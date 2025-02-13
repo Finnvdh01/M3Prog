6d7937ae25057e8182f1bfa75a7fc247351e76bd
Finnvdh01 <39246@ma-web.nl>
2025-02-13 12:36:52 +0100

    commit 9

---
+++ m3prog_project/public/01/status.php
@@ -0,0 +1,24 @@
+<?php
+$status = " <br> online";
+echo $status;
+$status = "<br> available";
+echo $status;
+$status = "<br> busy";
+echo $status;
+$status = "<br> away";
+echo $status;
+$status = "<br> offline";
+echo $status;
+
+$prijs = 20.50;
+echo "<br>", $prijs;
+$prijs = 50.00; 
+echo "<br>", $prijs;
+$prijs = 100.00;    
+echo "<br>", $prijs;
+$prijs = 150.00;    
+echo "<br>", $prijs;
+$prijs = 200.00;
+echo "<br>", $prijs;
+
+?>
