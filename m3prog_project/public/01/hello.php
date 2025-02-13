c7cb8b584c2e57649a11ef6523667e1c48eeebe0
Finnvdh01 <39246@ma-web.nl>
2025-02-13 11:59:35 +0100

    commit 4

--- m3prog_project/public/01/hello.php
+++ m3prog_project/public/01/hello.php
@@ -1,4 +1,7 @@
 <?php
 echo "Hello World!";
+echo "favoriete game: R6";
+echo "favorite film: fast and the furious";
+echo "klas: SD1C"
 
 ?>
---
+++ m3prog_project/public/index.php
@@ -0,0 +1,16 @@
+<?php
+echo "<h1>Defqon 1</h1>";
+
+echo "<p>Defqon.1 is een van de grootste en bekendste hardstyle- en hardcorefestivals ter wereld.
+ Het wordt jaarlijks georganiseerd door Q-dance en vindt plaats in Biddinghuizen, Nederland.
+  Het festival staat bekend om zijn indrukwekkende podia, vette lichtshows en keiharde muziekstijlen zoals hardstyle, rawstyle, hardcore, uptempo en terror.</p>";
+
+echo "<img src='images/artwork-440x220.jpg' alt='Defqon 1'>";
+
+echo "<h1>Thunderdome</h1>";
+
+echo "<p>Thunderdome is een van de meest legendarische hardcore-evenementen en een iconisch merk binnen de gabbercultuur.
+ Het werd in de jaren '90 opgericht door ID&T en heeft hardcoremuziek wereldwijd op de kaart gezet.</p>";
+
+echo "<img src='images/images (2).jpg' alt='Thunderdome'>";
+?>
