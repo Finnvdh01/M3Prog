5f79612b615f0185c503673ef08a64298e734670
Finnvdh01 <39246@ma-web.nl>
2025-02-13 12:41:16 +0100

    commit 10

---
+++ m3prog_project/public/01/variabelennaam.php
@@ -0,0 +1,24 @@
+
+<?php 
+$auto2 = "Audi";
+echo $auto2."<br>";
+$_naam = "BMW";
+echo $_naam."<br>";
+$auto = "Mercedes";
+echo $auto."<br>";
+//$auto/onderdelen = "Volkswagen";
+//echo $auto/onderdelen."<br>";
+//$huis&tuin = "Opel";
+//echo $huis&tuin."<br>";
+$naw = "Fiat";
+echo $naw."<br>";
+//$22 = "Peugeot";
+//echo $22."<br>";
+$$auto = "Renault";
+echo $$auto."<br>";
+$kleurVanAuto = "Geel";
+echo $kleurVanAuto."<br>";
+//$software-dev
+//echo $software-dev."<br>";
+?>
+
