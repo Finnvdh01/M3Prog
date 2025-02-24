5d9d13d22527b9283a5e48d0be5cda3ab01f24a2
Finnvdh01 <39246@ma-web.nl>
2025-02-24 12:14:33 +0100

    commit 13

---
+++ m3prog_project/public/02/simplecalc.php
@@ -0,0 +1,26 @@
+<?php 
+$som1 = 2 + 2;
+echo $som1;
+echo "<br>";
+$som2 = 5 * 5;
+echo $som2;
+echo "<br>";
+$som3 = 100/10;
+echo $som3;
+echo "<br>";
+$som4 = 60 - 25;
+echo $som4;
+echo "<br>";
+
+$varsom1 = $som1 * $som2;
+echo $varsom1;
+echo "<br>";
+$varsom2 = $som4 / $som3;
+echo $varsom2;
+echo "<br>";
+$varsom3 = $varsom1 * $som4 / $varsom2 - $som2;	
+echo $varsom3;
+echo "<br>";
+
+
+?>
