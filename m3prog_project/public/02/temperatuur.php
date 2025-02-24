d6166102676d8628402e04c5cf2b5cf68b909249
Finnvdh01 <39246@ma-web.nl>
2025-02-24 13:45:01 +0100

    commit 15

---
+++ m3prog_project/public/02/temperatuur.php
@@ -0,0 +1,9 @@
+<?php 
+$fahrenheit = 74;
+$celsius = ($fahrenheit - 32) / 1.8;
+echo "{$fahrenheit} graden Fahrenheit = ${$celcius} graden Celcius <br>";
+
+$celsius = 23;
+$fahrenheit = $celsius * 1.8 + 32; 
+echo "{$celsius} graden Celcius = ${$fahrenheit} graden Fahrenheit <br>";
+?>
