79a72e15ae162764bcd8f40aaa4e3c8e5372bed2
Finnvdh01 <39246@ma-web.nl>
2025-02-27 12:50:16 +0100

    commit 18

---
+++ m3prog_project/public/02/plak.php
@@ -0,0 +1,11 @@
+<?php 
+$standaardString = 'hello world<br>';
+$extraString = "extra world<br>";
+echo $standaardString;      
+echo $extraString;
+$voornaam = 'luigi';
+$achternaam = 'mario';
+$fullname = $voornaam $achternaam;
+echo $fullname;
+echo '<br>';
+?>
