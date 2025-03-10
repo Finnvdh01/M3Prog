93d6eebd861f49e0449ae1638e9848d152280b91
Finnvdh01 <39246@ma-web.nl>
2025-03-10 13:08:34 +0100

    commit 27

---
+++ m3prog_project/public/03/elseif.php
@@ -0,0 +1,30 @@
+<?php 
+$stoplichtkleur = 'R';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    if()//als Groen
+    {
+       echo <button style='background-color:#FF0000; height:25px; width:25px;'/>
+    }
+    else if()//als orangje
+    {
+       echo <button style='background-color:#FF0000;height:25px;width:25px;'/>
+    }
+    else//als rood
+    { 
+        echo <button style='background-color:#FF0000;height:25px;width:25px;'/>
+    }
+
+    
+    ?>
+</body>
+</html>
