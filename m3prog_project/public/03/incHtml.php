9d8cd12a6f7d331d6a5a454b7b6074c0b074c074
Finnvdh01 <39246@ma-web.nl>
2025-03-17 13:53:14 +0100

    commit 29

---
+++ m3prog_project/public/03/incHtml.php
@@ -0,0 +1,24 @@
+<?php
+$loggedin = false;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    if(isSet($_GET['loggedin']))
+    {
+       $loggedin=true;
+    }
+    else
+    {
+        include "views/home.html";
+    }
+    ?>
+</body>
+</html>
